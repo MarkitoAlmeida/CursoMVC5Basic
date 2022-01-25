f5855a13be19a56434ca240bfcd28f38223ebced
Marcos Almeida <mpalmeida88@gmail.com>
2022-01-25 10:14:18 +0000

    Add project files.

---
+++ CursoMVC5Basic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CursoMVC5Basic.Startup))]
+namespace CursoMVC5Basic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
